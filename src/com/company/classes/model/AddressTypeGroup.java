18dca6e2d021bbd0c3348d5b2be2414a8f3471b5
pitkw <pitkwiecien@gmail.com>
2022-05-21 14:12:33 +0200

    Added Address Types

---
+++ src/com/company/classes/model/AddressTypeGroup.java
@@ -0,0 +1,4 @@
+package com.company.classes.model;
+
+public interface AddressTypeGroup {
+}
---
+++ src/com/company/classes/model/CompanyAddressType.java
@@ -0,0 +1,5 @@
+package com.company.classes.model;
+
+public enum CompanyAddressType implements AddressTypeGroup{
+    BILLING, COMPANY, SHIPPING,
+}
